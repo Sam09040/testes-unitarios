fdb31ed47567a8e8caeed4d8913857b05829a73e
Guilherme <guilherme.cruz@usp.br>
2024-11-08 11:11:02 -0300

    adjust error text

--- tests/appTest.py
+++ tests/appTest.py
@@ -7,6 +7,6 @@ class AppTest(unittest.TestCase):
 
     def test_print_health_check(self):
         response = self.app.get('/health-check')
-        self.assertEqual(200, response.status_code, "Deu ruim no test_http_code!")
+        self.assertEqual(200, response.status_code, "Erro no test_http_code!")
         self.assertEqual("<h1>Hello, I'm Alive!</h1>", response.get_data(as_text=True)
-                          , "Deu Ruim no test_print_health_check!")
+                          , "Erro no test_print_health_check!")
