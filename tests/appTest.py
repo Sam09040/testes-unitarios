18901cb14bcf5f6c05508e4731219ad86f19addf
sam09040 <jpalmeida0904@gmail.com>
2026-03-31 21:00:53 -0300

    Testes em 91%

--- tests/appTest.py
+++ tests/appTest.py
@@ -1,12 +1,26 @@
 import unittest
+
 from app.app import app
+
 
 class AppTest(unittest.TestCase):
     def setUp(self):
         self.app = app.test_client()
 
     def test_print_health_check(self):
-        response = self.app.get('/health-check')
+        response = self.app.get("/health-check")
         self.assertEqual(200, response.status_code, "Erro no test_http_code!")
-        self.assertEqual("<h1>Hello, I'm Alive!</h1>", response.get_data(as_text=True)
-                          , "Erro no test_print_health_check!")
+        self.assertEqual(
+            "<h1>Hello, I'm Alive!</h1>",
+            response.get_data(as_text=True),
+            "Erro no test_print_health_check!",
+        )
+
+    def test_print_hello_success(self):
+        response = self.app.get("/hello", query_string={"name": "Sam"})
+        self.assertEqual(200, response.status_code, "Erro no test_print_hello_success!")
+        self.assertEqual(
+            "Hello, Sam!",
+            response.get_data(as_text=True),
+            "Erro no test_print_hello_success!",
+        )
